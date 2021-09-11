5aee24f7030e138b11323df20fd61ae878f8476a
PENGUINLIONG <admin@penguinliong.moe>
2021-09-11 22:59:08 +0800

    Disable optimization to avoid weird things

--- Run-Android.ps1
+++ Run-Android.ps1
@@ -7,8 +7,8 @@ if (-not(Test-Path "build-android-aarch64")) {
 }
 
 Push-Location "build-android-aarch64"
-cmake -DCMAKE_TOOLCHAIN_FILE="$env:ANDROID_NDK/build/cmake/android.toolchain.cmake" -DANDROID_ABI="arm64-v8a" -DANDROID_PLATFORM=android-29 -G "Ninja" ..
-cmake --build . -t $AppName
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE="$env:ANDROID_NDK/build/cmake/android.toolchain.cmake" -DANDROID_ABI="arm64-v8a" -DANDROID_PLATFORM=android-29 -G "Ninja" ..
+cmake --build . -t $AppName 
 
 adb push ./assets /data/local/tmp/graphi-t
 adb push ./bin /data/local/tmp/graphi-t
