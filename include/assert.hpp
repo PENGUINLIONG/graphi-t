86f54bff6aa1edd334134776be3741f99c9e044d
PENGUINLIONG <admin@penguinliong.moe>
2022-01-29 08:02:15 +0800

    Mark assertion and hard failures as `noreturn`

--- include/assert.hpp
+++ include/assert.hpp
@@ -16,17 +16,25 @@ public:
 
 template<typename ... TArgs>
 inline void assert(bool pred, const TArgs& ... args) {
+#ifndef NDEBUG
   if (!pred) {
     throw AssertionFailedException(liong::util::format(args ...));
   }
+#endif
 }
+[[noreturn]]
 template<typename ... TArgs>
 inline void panic(const TArgs& ... args) {
   assert<TArgs ...>(false, args ...);
 }
+[[noreturn]]
 template<typename ... TArgs>
 inline void unreachable(const TArgs& ... args) {
   assert<const char*, TArgs ...>(false, "reached unreachable code: ", args ...);
+}
+[[noreturn]]
+inline void unimplemented() {
+  assert<const char*, TArgs ...>(false, "reached unimplemented path");
 }
 
 } // namespace liong
