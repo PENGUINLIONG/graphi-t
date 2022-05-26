d2d340147b6728a28323780d3ecd427b748b277b
PENGUINLIONG <admin@penguinliong.moe>
2022-05-26 10:42:17 +0800

    Geometry tools

--- include/gft/geom.hpp
+++ include/gft/geom.hpp
@@ -1,6 +1,7 @@
 // Geometry algorithms all in right-hand-side systems.
 // @PENGUINLIONG
 #pragma once
+#include <vector>
 #include "glm/glm.hpp"
 
 namespace liong {
@@ -11,24 +12,48 @@ struct Ray {
   glm::vec3 v;
 };
 struct Triangle {
-  glm::vec3 p;
-  glm::vec3 u;
-  glm::vec3 v;
+  glm::vec3 a;
+  glm::vec3 b;
+  glm::vec3 c;
 };
 struct Aabb {
   glm::vec3 min;
   glm::vec3 max;
+
+  constexpr glm::vec3 center() const {
+    return max * 0.5f + min * 0.5f;
+  }
+  constexpr glm::vec3 size() const {
+    return max - min;
+  }
+
+  static constexpr Aabb from_min_max(
+    const glm::vec3& min,
+    const glm::vec3& max
+  ) {
+    return Aabb { min, max };
+  }
+  static constexpr Aabb from_center_size(
+    const glm::vec3& center,
+    const glm::vec3& size
+  ) {
+    return Aabb { center - 0.5f * size, center + 0.5f * size };
+  }
 };
 struct Sphere {
   glm::vec3 p;
   float r;
 };
 struct Tetrahedron {
-  glm::vec3 p;
   glm::vec3 a;
   glm::vec3 b;
   glm::vec3 c;
   glm::vec3 d;
+};
+struct Plane {
+  glm::vec3 n;
+  glm::vec3 u;
+  glm::vec3 v;
 };
 
 enum Facing {
@@ -57,6 +82,20 @@ extern bool contains_point_tet(const Tetrahedron& tet, const glm::vec3& point);
 extern bool intersect_tri(const Triangle& tri1, const Triangle& tri2);
 extern bool intersect_aabb_tri(const Triangle& tri, const Aabb& aabb);
 extern bool intersect_aabb(const Aabb& aabb1, const Aabb& aabb2);
+
+extern void split_tet2tris(const Tetrahedron& tet, std::vector<Triangle>& tris);
+extern void split_aabb2tets(const Aabb& aabb, std::vector<Tetrahedron>& tets);
+
+extern void subdivide_aabb(
+  const Aabb& aabb,
+  const glm::uvec3& nslice,
+  std::vector<Aabb>& out
+);
+extern void tile_aabb_ceil(
+  const Aabb& aabb,
+  const glm::vec3& tile_size,
+  std::vector<Aabb>& out
+);
 
 } // namespace geom
 } // namespace liong
--- src/gft/geom.cpp
+++ src/gft/geom.cpp
@@ -18,15 +18,111 @@ bool contains_point_sphere(const Sphere& sphere, const vec3& point) {
   return (point - sphere.p).length() <= sphere.r;
 }
 bool contains_point_tet(const Tetrahedron& tet, const vec3& point) {
-  vec3 v = point - tet.p;
-  float al = dot(v, tet.a) / dot(tet.a, tet.a);
-  float bl = dot(v, tet.b) / dot(tet.b, tet.b);
-  float cl = dot(v, tet.c) / dot(tet.c, tet.c);
-  float dl = dot(v, tet.d) / dot(tet.d, tet.d);
+  vec3 p = (tet.a + tet.b + tet.c + tet.d) * 0.5f;
+  vec3 v = point - p;
+  vec3 pa = tet.a - p;
+  vec3 pb = tet.b - p;
+  vec3 pc = tet.c - p;
+  vec3 pd = tet.d - p;
+  float al = dot(v, pa) / dot(pa, pa);
+  float bl = dot(v, pb) / dot(pb, pb);
+  float cl = dot(v, pc) / dot(pc, pc);
+  float dl = dot(v, pd) / dot(pd, pd);
   if (al > 1.0f || bl > 1.0f || cl > 1.0f || dl > 1.0f) {
     return false;
   }
   return true;
+}
+
+
+
+void split_tet2tris(const Tetrahedron& tet, std::vector<Triangle>& out) {
+  Triangle t0 { tet.a, tet.b, tet.c };
+  Triangle t1 { tet.a, tet.b, tet.d };
+  Triangle t2 { tet.a, tet.c, tet.d };
+  Triangle t3 { tet.b, tet.c, tet.d };
+
+  out.emplace_back(std::move(t0));
+  out.emplace_back(std::move(t1));
+  out.emplace_back(std::move(t2));
+  out.emplace_back(std::move(t3));
+}
+
+void split_aabb2tets(const Aabb& aabb, std::vector<Tetrahedron>& out) {
+  // Any cube can be split into five tetrahedra and in this function we split
+  // the AABB in a hard-coded pattern for simplicity.
+  //
+  //      A___________B
+  //      /|         /|
+  //     / |        / |
+  //   D/__|______C/  |   Y
+  //    |  |       |  |   |
+  //    | E|_______|_F|   |____X
+  //    | /        | /   /
+  //    |/_________|/   Z
+  //    H          G
+  //
+  // ABDE, BCDG, DEGH, BEGF, BDEG
+
+  glm::vec3 a(aabb.min.x, aabb.max.y, aabb.min.z);
+  glm::vec3 b(aabb.max.x, aabb.max.y, aabb.min.z);
+  glm::vec3 c(aabb.max.x, aabb.max.y, aabb.max.z);
+  glm::vec3 d(aabb.min.x, aabb.max.y, aabb.max.z);
+  glm::vec3 e(aabb.min.x, aabb.min.y, aabb.min.z);
+  glm::vec3 f(aabb.max.x, aabb.min.y, aabb.min.z);
+  glm::vec3 g(aabb.max.x, aabb.min.y, aabb.max.z);
+  glm::vec3 h(aabb.min.x, aabb.min.y, aabb.max.z);
+
+  Tetrahedron t0 { a, b, d, e };
+  Tetrahedron t1 { b, c, d, g };
+  Tetrahedron t2 { d, e, g, h };
+  Tetrahedron t3 { b, e, g, f };
+  Tetrahedron t4 { b, d, e, g };
+
+  out.emplace_back(std::move(t0));
+  out.emplace_back(std::move(t1));
+  out.emplace_back(std::move(t2));
+  out.emplace_back(std::move(t3));
+  out.emplace_back(std::move(t4));
+}
+
+void subdivide_aabb(
+  const Aabb& aabb,
+  const glm::uvec3& nslice,
+  std::vector<Aabb>& out
+) {
+  glm::vec3 size = aabb.size() / glm::vec3(nslice);
+
+  for (uint32_t z = 0; z < nslice.z; ++z) {
+    float z_min = aabb.min.z + z * size.z;
+    float z_max = z + 1 == nslice.y ?
+      aabb.max.z : aabb.min.z + (z + 1) * size.z;
+    for (uint32_t y = 0; y < nslice.y; ++y) {
+      float y_min = aabb.min.y + y * size.y;
+      float y_max = y + 1 == nslice.y ?
+        aabb.max.y : aabb.min.y + (y + 1) * size.y;
+      for (uint32_t x = 0; x < nslice.x; ++x) {
+        float x_min = aabb.min.x + x * size.x;
+        float x_max = x + 1 == nslice.x ?
+          aabb.max.x : aabb.min.x + (x + 1) * size.x;
+
+        Aabb aabb2 {};
+        aabb2.min = glm::vec3(x_min, y_min, z_min);
+        aabb2.max = glm::vec3(x_max, y_max, z_max);
+      }
+    }
+  }
+}
+
+void tile_aabb_ceil(
+  const Aabb& aabb,
+  const glm::vec3& tile_size,
+  std::vector<Aabb>& out
+) {
+  glm::uvec3 nslice(glm::ceil(aabb.size() / tile_size));
+  glm::vec3 size2 = glm::vec3(nslice) * tile_size;
+  Aabb aabb2 = Aabb::from_center_size(aabb.center(), size2);
+  subdivide_aabb(aabb, nslice, out);
 }
 
 } // namespace geom
